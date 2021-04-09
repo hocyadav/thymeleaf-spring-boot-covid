2806e1c2be56f0126d6bc6281f48c9e521914138
Hariom Yadav <hoc.yadav@gmail.com>
2021-04-09 13:20:21 +0530

    updated

---
+++ src/main/java/com/hari/springsecurity/config/AppConfig.java
@@ -0,0 +1,23 @@
+package com.hari.springsecurity.config;
+
+import lombok.*;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author Hariom Yadav
+ * @create 08-04-2021
+ */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+@ConfigurationProperties(prefix = "app-config")
+public class AppConfig {
+    String simpleKey;
+    String allCountriesURL;
+    String singleCountryURL;
+    String singleCountryHistoryURL;
+}
